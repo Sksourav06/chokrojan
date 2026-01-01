014c16a35da4b8eddea5dc9ba6a38cf5ce23af6a
Sourav <souravkumarjoarder@gmail.com>
2026-01-01 11:52:44 +0600

    commits logout fix

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -43,7 +43,7 @@ class LoginController extends Controller
         Auth::logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
-        return redirect('/');
+        return redirect('/login');
     }
 
     public function sendLoginCode(Request $request)
